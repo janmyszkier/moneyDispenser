ab6b5b40dd420dda33b39b00ddb4c74ca9d68bda
Jan Myszkier <janmyszkier@gmail.com>
2019-06-14 18:11:37 +0200

    add tests for requested examples, add InvalidArgumentException to support all cases sent earlier

---
+++ src/Service/MoneyDispenser/InvalidArgumentException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Service\MoneyDispenser;
+
+class InvalidArgumentException extends \Exception
+{
+
+}
--- tests/Service/MoneyDispenserTest.php
+++ tests/Service/MoneyDispenserTest.php
@@ -3,6 +3,9 @@
 
 use App\Service\MoneyDispenser;
 use App\Entity\BankNote\Collection;
+use App\Entity\PolishZlotyBankNote;
+use App\Entity\BankNote\NoteUnavailableException;
+use App\Service\MoneyDispenser\InvalidArgumentException;
 
 class MoneyDispenserTest extends \PHPUnit\Framework\TestCase
 {
@@ -16,5 +19,55 @@ class MoneyDispenserTest extends \PHPUnit\Framework\TestCase
          */
         //$this->assertInstanceOf(Collection::class,get_class($bankNotes));
         $this->assertEquals(Collection::class,get_class($bankNotes));
+    }
+
+    public function test30Amount()
+    {
+        $moneyDispenser = new MoneyDispenser();
+        $bankNotes = $moneyDispenser->retrieveAmount(30);
+
+        $expectedCollection = new Collection();
+        $expectedCollection->addBankNote(new PolishZlotyBankNote(20));
+        $expectedCollection->addBankNote(new PolishZlotyBankNote(10));
+
+        $this->assertEquals($expectedCollection, $bankNotes);
+    }
+
+    public function test80Amount()
+    {
+        $moneyDispenser = new MoneyDispenser();
+        $bankNotes = $moneyDispenser->retrieveAmount(80);
+
+        $expectedCollection = new Collection();
+        $expectedCollection->addBankNote(new PolishZlotyBankNote(50));
+        $expectedCollection->addBankNote(new PolishZlotyBankNote(20));
+        $expectedCollection->addBankNote(new PolishZlotyBankNote(10));
+
+        $this->assertEquals($expectedCollection, $bankNotes);
+    }
+
+    public function test125Amount()
+    {
+        $this->expectException(NoteUnavailableException::class);
+
+        $moneyDispenser = new MoneyDispenser();
+        $bankNotes = $moneyDispenser->retrieveAmount(125);
+    }
+
+    public function testMinus130Amount()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $moneyDispenser = new MoneyDispenser();
+        $bankNotes = $moneyDispenser->retrieveAmount(-130);
+    }
+
+    public function testNullAmount()
+    {
+        $moneyDispenser = new MoneyDispenser();
+        $bankNotes = $moneyDispenser->retrieveAmount(null);
+
+        $expectedCollection = null;
+        $this->assertEquals($expectedCollection,$bankNotes);
     }
 }
