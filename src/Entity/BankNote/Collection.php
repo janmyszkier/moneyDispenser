8906ded893188185f3483e94d98d0e4ce3e0b7c5
Jan Myszkier <janmyszkier@gmail.com>
2019-06-14 17:26:02 +0200

    create BankNoteCollection class that allows you to use Bank Notes only

---
+++ src/Entity/BankNote/Collection.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Entity\BankNote;
+
+use App\Entity\BankNoteInterface;
+
+class Collection implements \IteratorAggregate
+{
+    protected $set = [];
+
+    public function addBankNote(BankNoteInterface $bankNote){
+        $this->set[] = $bankNote;
+    }
+
+    public function getIterator()
+    {
+        return $this->set;
+    }
+}
